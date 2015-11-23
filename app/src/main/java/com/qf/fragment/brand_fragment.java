cbf806176bb6268d697c5052d2d3c85456c054fe
Administrator <1069008110@qq.com>
2015-11-23 19:50:53 +0800

    添加adapter

--- app/src/main/java/com/qf/fragment/brand_fragment.java
+++ app/src/main/java/com/qf/fragment/brand_fragment.java
@@ -1,12 +1,10 @@
 package com.qf.fragment;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.administrator.project.R;
 
 /**
@@ -19,11 +17,10 @@ public class brand_fragment extends Fragment {
 
         this.context=context;
     }
-////////////////////////
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+     View view=inflater.inflate(R.layout.layout1,container,false);
 
-        return inflater.inflate(R.layout.layout1,container,false);
+        return view;
     }
 }
